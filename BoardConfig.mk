4fbf55547ec15e33374c969883448df85e4ece6f
[psychicgirl2020] <[anairachu@gmail.com]>
2020-07-19 04:26:44 +0000

    Dummy Tree

---
+++ BoardConfig.mk
@@ -0,0 +1,5 @@
+DEVICE_PATH := device/samsung/j2y18lte
+BOARD_VENDOR := samsung
+
+
+-include vendor/samsung/j2y18lte/BoardConfigVendor.mk
---
+++ lineage_j2y18lte.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from j2y18lte device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := j2y18lte
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_NAME := lineage_j2y18lte
+PRODUCT_MODEL := SM-J250F
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_VENDOR := samsung
+TARGET_VENDOR_PRODUCT_NAME := j2y18lte
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j2y18ltejx-user 7.1.1 NMF26X J250FXXU2ASK1 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := samsung/j2y18ltejx/j2y18lte:7.1.1/NMF26X/J250FXXU2ASK1:user/release-keys
